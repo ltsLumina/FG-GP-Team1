388272a3e636b340482e4d7006f917cd9330eae9
OscarAnderlind <anderlindoscar@gmail.com>
2024-11-05 14:15:23 +0100

    boids in oscar

---
+++ Assets/_Project/Runtime/_Scripts/BoidStuff/Boid.cs
@@ -0,0 +1,247 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Boid : MonoBehaviour
+{
+
+    private float moveSpeed;
+    private float currentMoveSpeed;
+    private float turnSpeed;
+    private float currentTurnSpeed;
+    private float wallContext;
+
+    private float seperationContext;
+    private float seperationStrength;
+
+    private float alignmentContext;
+    private float alignmentStrength;
+
+    private float cohesionContext;
+    private float cohesionStrength;
+
+    private float fleeingMoveSpeedMult;
+    private float fleeContext;
+    private float fleeTime;
+
+    private bool fleeing = false;
+
+    private BoidSpawner spawner;
+
+    LayerMask groundLayer = 1 << 3;
+
+    private Vector3 targetDir;
+
+    [SerializeField] private List<Boid> boids = new List<Boid>();
+
+    private float timer;
+
+    private float updateTimer = 0.5f;
+    
+
+    private void Start()
+    {
+        transform.right = new Vector3(Random.Range(-1f,1f), 0, Random.Range(-1f, 1f)).normalized;
+
+        updateTimer = Random.Range(0, updateTimer);
+
+        float maxContext = 0;
+
+        if(seperationContext > maxContext)
+        {
+            maxContext = seperationContext;
+        }
+        if(alignmentContext > maxContext)
+        {
+            maxContext = alignmentContext;
+        }
+        if(cohesionContext > maxContext)
+        {
+            maxContext = cohesionContext;
+        }
+         
+        GetComponent<SphereCollider>().radius = maxContext;
+
+        currentMoveSpeed = moveSpeed;
+
+        currentTurnSpeed = turnSpeed;
+
+
+        UpdateBehaviour();
+    }
+
+    private void FixedUpdate()
+    {
+
+        timer -= Time.deltaTime;
+        if(timer < 0)
+        {
+            UpdateBehaviour();
+            timer = updateTimer;
+        }
+
+        transform.right = Vector3.MoveTowards(transform.right, targetDir, currentTurnSpeed * Time.deltaTime);
+
+        transform.position += transform.right * currentMoveSpeed * Time.deltaTime;
+
+    }
+
+    private void UpdateBehaviour()
+    {
+
+        if (fleeing)
+        {
+            return;
+        }
+
+        Vector3 sumForces = Vector3.zero;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.right, out hit, wallContext, groundLayer))
+        {
+            //Debug.DrawRay(transform.position, transform.right * hit.distance, Color.red);
+            sumForces = transform.right * -1;
+        }
+        else
+        {
+            //Debug.DrawRay(transform.position, transform.right * wallContext, Color.green);
+            sumForces += Seperation() * seperationStrength;
+            sumForces += Alignment() * alignmentStrength;
+            sumForces += Cohesion() * cohesionStrength;
+        }
+
+        targetDir = sumForces;
+    }
+
+    private Vector3 Seperation()
+    {
+        Vector3 seperationVector = Vector3.zero;
+
+        for (int i = 0; i < boids.Count; i++)
+        {
+            if(Vector3.Distance(transform.position, boids[i].transform.position) < seperationContext)
+            {
+                Vector3 tempVector = Vector3.zero;
+                tempVector += transform.position - boids[i].transform.position;
+                tempVector /= Mathf.Pow(Vector3.Distance(transform.position, boids[i].transform.position), 2);
+                seperationVector += tempVector;
+            }
+        }
+
+        return seperationVector.normalized;
+
+    }
+
+    private Vector3 Alignment()
+    {
+        Vector3 alignmentVector = Vector3.zero;
+
+        for(int i = 0; i < boids.Count; i++)
+        {
+            if (Vector3.Distance(transform.position, boids[i].transform.position) < alignmentContext)
+            {
+                alignmentVector += boids[i].TargetDir;
+            } 
+        }
+
+        alignmentVector /= boids.Count;
+        return alignmentVector.normalized;
+    }
+
+    private Vector3 Cohesion()
+    {
+        Vector3 cohesionVector = Vector3.zero;
+
+        for (int i = 0; i < boids.Count; i++)
+        {
+            if (Vector3.Distance(transform.position, boids[i].transform.position) < cohesionContext)
+            {
+                cohesionVector += boids[i].transform.position - transform.position;
+            }
+        }
+
+        cohesionVector /= boids.Count;
+
+        return (cohesionVector).normalized;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Boid"))
+        {
+           spawner.Boids.TryGetValue(other.gameObject, out Boid boid);
+           if(boid != null)
+           {
+               boids.Add(boid);
+           }
+
+        }
+        else if (other.CompareTag("Player"))
+        {
+            StopAllCoroutines();
+            StartCoroutine(Fleeing(other));
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Boid"))
+        {
+            spawner.Boids.TryGetValue(other.gameObject, out Boid boid);
+            if (boid != null)
+            {
+                boids.Remove(boid);
+            }
+        }
+    }
+
+    private IEnumerator Fleeing(Collider other)
+    {
+        fleeing = true;
+        targetDir = transform.position - other.transform.position;
+        currentMoveSpeed = moveSpeed * fleeingMoveSpeedMult;
+        yield return new WaitForSeconds(fleeTime);
+        fleeing = false;
+        currentMoveSpeed = moveSpeed;
+    }
+
+    public Vector3 TargetDir
+    {
+        get => targetDir;
+    }
+
+    public float MoveSpeed
+    {
+        set => moveSpeed = value;
+    }
+
+    public float TurnSpeed
+    {
+        set => turnSpeed = value;
+    }
+
+    public float WallContext
+    {
+        set => wallContext = value;
+    }
+
+
+
+    public LayerMask GroundLayer
+    {
+        set => groundLayer = value;
+    }
+    public float SeperationContext { get => seperationContext; set => seperationContext = value; }
+    public float SeperationStrength { get => seperationStrength; set => seperationStrength = value; }
+    public float AlignmentContext { get => alignmentContext; set => alignmentContext = value; }
+    public float AlignmentStrength { get => alignmentStrength; set => alignmentStrength = value; }
+    public float CohesionContext { get => cohesionContext; set => cohesionContext = value; }
+    public float CohesionStrength { get => cohesionStrength; set => cohesionStrength = value; }
+    public float FleeContext { get => fleeContext; set => fleeContext = value; }
+    public float FleeTime { get => fleeTime; set => fleeTime = value; }
+    public float FleeingMoveSpeedMult { get => fleeingMoveSpeedMult; set => fleeingMoveSpeedMult = value; }
+    public BoidSpawner Spawner {  get => spawner; set => spawner = value; }
+}
---
+++ Assets/_Project/Runtime/_Scripts/BoidStuff/BoidSpawner.cs
@@ -0,0 +1,70 @@
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+
+public class BoidSpawner : MonoBehaviour
+{
+    [Header("Spawning")]
+    [SerializeField] private int amountOfBoids = 300;
+    [SerializeField] private int spawnRangeX = 100;
+    [SerializeField] private int spawnRangeY = 50;
+    [SerializeField] private int spawnRangeZ = 100;
+
+    [SerializeField] private BoidDataSO boidDataSO;
+
+    private Dictionary<GameObject, Boid> boids = new Dictionary<GameObject, Boid>();
+
+    private void Start()
+    {
+        SpawnFish();
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0, 1, 0, 0.2f);
+        Gizmos.DrawCube(transform.position, new Vector3(spawnRangeX, spawnRangeY, spawnRangeZ));
+    }
+
+    private void SpawnFish()
+    {
+        for (int i = 0; i < amountOfBoids; i++)
+        {
+            GameObject fish = new GameObject("Fish");
+            fish.transform.position = new Vector3(transform.position.x + (Random.Range(-spawnRangeX, spawnRangeX) / 2), transform.position.y + (Random.Range(-spawnRangeY, spawnRangeY) / 2), transform.position.z + (Random.Range(-spawnRangeZ, spawnRangeZ) / 2));
+            fish.AddComponent<MeshFilter>().mesh = boidDataSO.model;
+            fish.AddComponent<MeshRenderer>().material = boidDataSO.material;
+            fish.transform.parent = transform;
+            var boid = fish.AddComponent<Boid>();
+            fish.AddComponent<SphereCollider>().isTrigger = true;
+            fish.tag = "Boid";
+            fish.AddComponent<Rigidbody>().useGravity = false;
+            boid.MoveSpeed = boidDataSO.moveSpeed;
+            boid.TurnSpeed = boidDataSO.turnSpeed;
+            boid.WallContext = boidDataSO.wallContext;
+            boid.SeperationStrength = boidDataSO.seperationStrength;
+            boid.SeperationContext = boidDataSO.seperationContext;
+
+            boid.AlignmentStrength = boidDataSO.alignmentStrength;
+            boid.AlignmentContext = boidDataSO.alignmentContext;
+
+            boid.CohesionStrength = boidDataSO.cohesionStrength;
+            boid.CohesionContext = boidDataSO.cohesionContext;
+
+            boid.FleeContext = boidDataSO.fleeContext;
+            boid.FleeingMoveSpeedMult = boidDataSO.fleeingMoveSpeedMult;
+            boid.FleeTime = boidDataSO.fleeTimeSeconds;
+
+            boid.Spawner = this;
+
+            boids.Add(fish, boid);
+        }
+    }
+    public Dictionary<GameObject, Boid> Boids
+    {
+        get => boids;
+    }
+
+}
