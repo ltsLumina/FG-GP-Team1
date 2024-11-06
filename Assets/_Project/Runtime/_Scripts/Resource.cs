fe26b2d34f90a903f8e17158bf719766fdd55ac5
Lumina <alexander.andrejeff@elev.ga.lbs.se>
2024-11-07 00:34:25 +0100

    Update Kelp Model

--- Assets/_Project/Runtime/_Scripts/Resource.cs
+++ Assets/_Project/Runtime/_Scripts/Resource.cs
@@ -40,11 +40,15 @@ public class Resource : MonoBehaviour, IGrabbable
 
     void OnEnable()
     {
-        onGrabbed  += ResetVelocity;
+        onGrabbed += () =>
+        {
+            SetMesh(true);
+            ResetVelocity();
+        };
         onReleased += () =>
         {
+            SetMesh(true);
             ResetVelocity();
-            transform.localScale = Vector3.one * 2;
         };
     }
 
@@ -77,8 +81,6 @@ public class Resource : MonoBehaviour, IGrabbable
 
     void Start()
     {
-        GetComponent<MeshRenderer>().materials[0].color = Item == IGrabbable.Items.Kelp ? Color.green : Color.yellow;
-
         if (Lifetime <= 5) Debug.LogWarning("Lifetime is set too low. Object will likely be destroyed before it has left the screen bounds.");
         Invoke(nameof(Destroy), Lifetime);
     }
@@ -86,23 +88,22 @@ public class Resource : MonoBehaviour, IGrabbable
     void Update()
     {
         if (!Grabbed) return;
-
+        
         var player = Find<Player>();
         var moveInput = player.GetComponentInChildren<InputManager>().MoveInput;
         var offset = new Vector3(moveInput.x * 3f, moveInput.y * 3f);
-
-        // TODO: THIS IS TEMPORARY FOR VISUAL INDICATION
-        if (moveInput == Vector2.zero)
-        {
-            offset  = new (3, 3);
-            transform.localScale = Vector3.one;
-        }
-        else
-        {
-            transform.localScale = Vector3.one * 2; 
-        }
+        if (moveInput == Vector2.zero) offset = new (2, 2);
 
         transform.position = player.transform.position + offset;
+    }
+    
+    void SetMesh(bool useGrabbedMesh)
+    {
+        var regularMesh = transform.GetChild(0);
+        var grabbedMesh = transform.GetChild(1);
+        
+        regularMesh.gameObject.SetActive(!useGrabbedMesh);
+        grabbedMesh.gameObject.SetActive(useGrabbedMesh);
     }
 
     void OnDrawGizmos()
