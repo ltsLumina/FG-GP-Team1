201a9ecd45d80f3128339e91508f2d9184beb673
Turner Monroe <me@turnercore.dev>
2024-11-06 19:30:40 +0100

    Slight Player Anim Change
    Just moved around some layers

--- Assets/_Project/Runtime/Resources/Player/PlayerAnimationStates.cs
+++ Assets/_Project/Runtime/Resources/Player/PlayerAnimationStates.cs
@@ -15,6 +15,36 @@ public class PlayerAnimationStates : MonoBehaviour
         input = transform.parent.GetComponentInChildren<InputManager>();
     }
 
+    public void Dash()
+    {
+        anim.SetTrigger("Dash");
+    }
+
+    public void Grab(GameObject grabbedObject)
+    {
+        anim.SetTrigger("Grab");
+    }
+
+    public void Release()
+    {
+        anim.SetTrigger("Release");
+    }
+
+    public void Die()
+    {
+        anim.SetTrigger("Die");
+    }
+
+    public void Spawn()
+    {
+        anim.SetTrigger("Spawn");
+    }
+
+    public void Stun()
+    {
+        anim.SetTrigger("Stun");
+    }
+
     private void Update()
     {
         // Set blend tree parameters for player movement based on input of new input system Player action Move
