114c37d1f8846b769bea2a764331e179dc734836
OscarAnderlind <anderlindoscar@gmail.com>
2024-11-08 18:38:27 +0100

    commit

---
+++ Assets/_Project/Runtime/_Scripts/JellyMovement.cs
@@ -0,0 +1,104 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Threading;
+using System.Collections;
+using DG.Tweening;
+
+public class JellyMovement : MonoBehaviour
+{
+
+    [SerializeField] private float moveCoolDown;
+    private float moveTimer = 0;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float moveScale;
+    private GameObject player;
+    private Vector2 moveDir;
+    private Vector2 playerDir;
+    private Vector3 movePos;
+    private Vector3 normalScale;
+    private bool movingToTarget = true;
+    [SerializeField] private float wallContext;
+    [SerializeField] LayerMask groundLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        moveTimer = moveCoolDown;
+        normalScale = transform.localScale;
+        FindTarget();
+    }
+
+    private void Update()
+    {
+
+        if (moveTimer < 0)
+        {
+            FindTarget();
+
+        }
+
+        if (movingToTarget)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, movePos, Time.deltaTime * moveSpeed * (Vector3.Distance(transform.position, movePos) + 0.1f));
+        }
+        else
+        {
+            transform.position += new Vector3(0, -2 * Time.deltaTime * Mathf.Min(Vector3.Distance(transform.position, movePos) + 0.1f,2), 0);
+        }
+
+
+        if (transform.position == movePos)
+        {
+            movingToTarget = false;
+        }
+
+        transform.localScale += new Vector3(0, 0.3f * -Time.deltaTime, 0);
+        moveTimer -= Time.deltaTime;
+
+    }
+
+    private void FindTarget()
+    {
+
+        transform.localScale = normalScale;
+        movingToTarget = true;
+        moveTimer = moveCoolDown;
+
+        float minMag = 10;
+        int moveIndex = 0;
+        List<Vector3> randDirs = new List<Vector3>();
+
+
+        for (int i = 0; i <= 6; i++)
+        {
+            randDirs.Add(Random.insideUnitCircle);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(transform.position, randDirs[i], out hit, 3, groundLayer))
+            {
+                Debug.DrawRay(transform.position, randDirs[i], Color.red, 2);
+                Debug.Log("hello");
+                randDirs[i] = new Vector3(-randDirs[i].x, randDirs[i].y, randDirs[i].z);
+                return;
+            }
+
+
+            float currentMag = ((Vector2)randDirs[i] - Vector2.up).magnitude;
+            if (currentMag < minMag)
+            {
+                minMag = currentMag;
+                moveIndex = i;
+            }
+        }
+
+
+        movePos = new Vector2(transform.position.x, transform.position.y) + (Vector2)randDirs[moveIndex] * moveScale;
+    }
+
+
+}
+
+
+//avoid wall / sub
