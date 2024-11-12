f2c43341fcf06493f553cf5ad2da13971dfaf1ea
Turner Monroe <me@turnercore.dev>
2024-11-12 21:15:27 +0100

    Battery Animations (again)

--- Assets/BatteryAnimation.cs
+++ Assets/BatteryAnimation.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class BatteryAnimation : MonoBehaviour
 {
+    Battery battery;
+    Animator anim;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        battery = GetComponent<Battery>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Update the animation state
+        anim.SetFloat("Charge", battery.Charge);
     }
 }
