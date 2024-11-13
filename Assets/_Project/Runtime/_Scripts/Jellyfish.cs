cad70c1fe17aa879991d6fb14387191ac861fab7
OscarAnderlind <anderlindoscar@gmail.com>
2024-11-13 21:41:55 +0100

    jellyfish audio

--- Assets/_Project/Runtime/_Scripts/Jellyfish.cs
+++ Assets/_Project/Runtime/_Scripts/Jellyfish.cs
@@ -13,6 +13,9 @@ public class Jellyfish : MonoBehaviour
     [SerializeField] int radius;
     
     SphereCollider col;
+
+    [Header("Audio")]
+    [SerializeField] FMODUnity.EventReference shockSound;
     
     void Start()
     {
@@ -36,6 +39,11 @@ public class Jellyfish : MonoBehaviour
             // Stun the player if they are not holding a battery.
             if (other.TryGetComponent(out Player player))
             {
+
+                var shock = FMODUnity.RuntimeManager.CreateInstance(shockSound);
+                FMODUnity.RuntimeManager.AttachInstanceToGameObject(shock, transform);
+                shock.start();
+
                 if (Player.HoldingResource(out Battery battery))
                 {
                     Debug.Log("Battery charged");
